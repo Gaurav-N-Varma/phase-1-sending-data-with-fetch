b2d710f8601d8ae7c8aa0dc167e2d2f8f3ed97e2
Gaurav <gaurav.narayan.varma@gmail.com>
2022-08-12 16:52:50 -0400

    commit tree 6dc732170a4662dae6795a98605e797fdb70a8f1

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       dogName: "Byron",
+//       dogBreed: "Poodle",
+//     }),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject);
+
+// function submitData(nameString, emailString){
+//     return(
+//         // Storing the user data in the API
+//         fetch('http://localhost:3000/users', {
+//             method: 'POST',
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "application/json",
+//             },
+//             body: JSON.stringify({
+//                 name: nameString,
+//                 email: emailString
+//             })
+//         })
+//         .then(response => response.json())
+
+//         // Printing the id number to the screen
+//         .then((object) => {
+//             const element = document.createElement('h1')
+//             element.textContent = object.id
+//             document.body.append(element)
+//         })
+//         // if the promise is a failure, print its message property
+//         .catch((err) => {
+//             alert(err.message)
+//             const errorElement = document.createElement('p')
+//             errorElement.textContent = err.message
+//             document.body.append(errorElement)
+//         })
+//     )
+// }
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
